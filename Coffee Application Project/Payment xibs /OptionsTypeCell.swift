d6165983335a5656632d849636bcf6048e67e7b0
saurav-sharma-iphtech <saurav.sharma@iphtechnologies.com>
2025-05-23 18:32:50 +0530

    update

--- Coffee Application Project/Payment xibs /OptionsTypeCell.swift
+++ Coffee Application Project/Payment xibs /OptionsTypeCell.swift
@@ -18,6 +18,9 @@ class OptionsTypeCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
+    
+    
+    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
